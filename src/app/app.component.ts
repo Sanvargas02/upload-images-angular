864751856b26cf268a2d5a5be31c7a9138843dfa
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-15 09:51:29 -0500

    Almacenando img y mostrando path en consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,35 +17,49 @@ export class AppComponent implements OnInit {
 
   //Método de arranque
   ngOnInit() {
-    this.getImagies();
+    // this.getImagies();
   }
 
   uploadImage($event: any) {
 
-    const file = $event.target.files[0]; //Apuntamos al input y luego los ficheros, elejimos el primero
-    console.log(file);
+    const files = $event.target.files; //Apuntamos al input y luego los ficheros
+    console.log(files);
 
     //Creamos una referencia al sitio de firebase
     // En la referencia se coloca el servicio y el path, aún si el path no exíste se puede declarar
-    const imgRef = ref(this.storage, `images/${file.name}`);
 
-    //Insertamos la referencia y el fichero que queremos subir.
-    //Nos retorna una promesa.
-    //Método para subir la imágen.
-    uploadBytes(imgRef, file)
-    .then((response) => {
+    // const imgRef = ref(this.storage, `images/${file.name}`);
+
+    //Creamos una forma de cargar todo el arreglo
+    //Utilizamos un ciclo for para recorrer el arreglo e insertar archivo por archivo adquiriendo su referencia
+    for(let file of files) {
+
+      //Creamos la referencia
+      const imgRef = ref(this.storage, `img/${file.name}`);
+
+      //Insertamos la referencia y el archivo en este caso imágen que queremos subir.
+      //Método para subir la imágen.
+      uploadBytes(imgRef, file) //Nos retorna una promesa
+      .then((response) => {
       console.log(response)
-      //Nos podemos plantear guardar el path a la imágen en una de las propiedades del objeto que estemos insertando en la base de datos Firestore, para luego cuando llamemos a los datos el objeto guardado podamos usar ese mismo path y con una función de storage pintar la imágen en pantalla.
-      //const fullPath = response.metadata.fullPath;
-      //console.log(fullPath);
-      //También se puede guardar la URL de descarga del un archivo directamente en el objeto que queremos guardar, aplicando el método getDownloadURL() con la referencia ref() del elemento en concreto.
-      this.getImagies();
-    } )
-    .catch( (error) => { console.log(error) } )
 
+      //Nos podemos plantear guardar el path a la imágen en una de las propiedades del objeto que estemos insertando en la base de datos Firestore, para luego cuando llamemos a los datos el objeto guardado podamos usar ese mismo path y con una función de storage pintar la imágen en pantalla.
+      const pathReference = response.metadata.fullPath;
+      console.log(pathReference);
+      //También se puede guardar la URL de descarga del un archivo directamente en el objeto que queremos guardar, aplicando el método getDownloadURL() con la referencia ref() del elemento en concreto.
+
+
+      // this.getImagies();
+      })
+      .catch( (error) => { console.log(error) } )
+
+    }
   }
 
+
+
   //Método para obtener imágenes
+
   getImagies() {
     //Se crea una referencia
     //Con el método ref() le decimos qué queremos, archivo, imagen, carpeta, en sí la dirección
