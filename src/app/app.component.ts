13966e468c2c6fbebfe8d943bb320e8d8dbb6b06
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-15 12:01:27 -0500

    Insertamos archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,25 @@ import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fir
 })
 export class AppComponent implements OnInit {
 
-  //Propiedad para guardad las url de descarga de las imágenes
+  //Propiedad para guardad las url de descarga de las imágenes, esta va a ser posteriormente una propiedad que añadiremos a nuestro objeto Place o Persona o Empleado etc.
   images: string[];
+
+  fullPaths: string[];
 
   constructor(private storage: Storage) {
     this.images = []; //Se inicializa la propiedad
+    this.fullPaths = []; // Inicializamos
    }
 
   //Método de arranque
   ngOnInit() {
-    // this.getImagies();
+    //this.getImagies();
   }
 
   uploadImage($event: any) {
 
     const files = $event.target.files; //Apuntamos al input y luego los ficheros
-    console.log(files);
+    //console.log(files);
 
     //Creamos una referencia al sitio de firebase
     // En la referencia se coloca el servicio y el path, aún si el path no exíste se puede declarar
@@ -34,52 +37,92 @@ export class AppComponent implements OnInit {
     //Utilizamos un ciclo for para recorrer el arreglo e insertar archivo por archivo adquiriendo su referencia
     for(let file of files) {
 
-      //Creamos la referencia
-      const imgRef = ref(this.storage, `img/${file.name}`);
+      //Creamos la referencia para guardar
+      const imgRef = ref(this.storage, `carpeta/${file.name}`);
 
       //Insertamos la referencia y el archivo en este caso imágen que queremos subir.
       //Método para subir la imágen.
       uploadBytes(imgRef, file) //Nos retorna una promesa
       .then((response) => {
-      console.log(response)
+      // console.log(response);
 
-      //Nos podemos plantear guardar el path a la imágen en una de las propiedades del objeto que estemos insertando en la base de datos Firestore, para luego cuando llamemos a los datos el objeto guardado podamos usar ese mismo path y con una función de storage pintar la imágen en pantalla.
-      const pathReference = response.metadata.fullPath;
-      console.log(pathReference);
+      //Nos podemos plantear guardar la url de descarga a la imágen en una de las propiedades del objeto que estemos insertando en la base de datos Firestore.
+
+      const fullPath = response.metadata.fullPath; //fullPath es candidato a ser guardado en Firestore, en un arreglo de fullPaths.
+
+      this.fullPaths.push(fullPath);
+
+      // console.log(pathReference);
       //También se puede guardar la URL de descarga del un archivo directamente en el objeto que queremos guardar, aplicando el método getDownloadURL() con la referencia ref() del elemento en concreto.
 
+      //Creamos referencia al path donde se guardó la img para traer el link de descarga
+      //const pathReference = ref(this.storage, fullPath);
+
+      //Es probable que el código que continúa sea mejor colocarlo en un método get
+
+      //Podemos usar then y cach pero la anidación se puede tornar complicada de leer.
+      //const url = await getDownloadURL(pathReference);
+      //console.log(url);
+
+      //Este arreglo con las url se convierte en nuestra propiedad
+      // this.images.push(url);
 
       // this.getImagies();
       })
       .catch( (error) => { console.log(error) } )
 
-    }
-  }
+    } //Fin del for
+
+    console.log(this.fullPaths); // fullPaths es la propiedad que voy a almacenar en mi objeto Place, Empleado etc.
+    this.getImagies();
+
+  } // fin del método
 
 
 
   //Método para obtener imágenes
 
   getImagies() {
+    console.log(this.fullPaths.length);
     //Se crea una referencia
     //Con el método ref() le decimos qué queremos, archivo, imagen, carpeta, en sí la dirección
-    const imagesRef = ref(this.storage, 'images');
+    // const imagesRef = ref(this.storage, 'images');
 
-    listAll(imagesRef)
-    .then( async response => {
+    //En este caso utilizamos el fullPaths como arreglo para mostrar las imágenes pero tenemos que tener en cuenta que se debe utilizar la propiedad que viene desde el objeto que traemos
 
-      console.log(response);
-
-      this.images = []; //Borramos el array antes de el for para que no se vayan sobre escribiendo.
-
-      for(let item of response.items) {
-        //Podemos usar then y cach pero la anidación se puede tornar complicada de leer.
-        const url = await getDownloadURL(item); // Obtenemos la URL de descarga de cada objeto del arreglo de items.
-        this.images.push(url); //insertamos en el arreglo images cada una de las url traidas desde firebase.
-      }
+    this.fullPaths.forEach(fullPaht => {
+      console.log(fullPaht);
     })
-    .catch( error => console.log(error) )
-  }
 
-}
+    //   //Creamos la referencia
+    //   const pathReference = ref(this.storage, fullPath);
+
+    //   //Utilizamos el método getDownloadUrl para obtener la url de descarga
+    //   getDownloadURL(pathReference)
+    //   .then(url => {
+    //     console.log(url);
+    //   })
+    //   .catch( error => {console.log(error)})
+
+    //   //Se guarda en un arreglo que se va a renderizar al final en el html
+    //   //this.images.push(url); //insertamos en el arreglo images cada una de las url traidas desde firebase.
+
+
+  //   listAll(imagesRef)
+  //   .then( async response => {
+
+  //     console.log(response);
+
+  //     this.images = []; //Borramos el array antes de el for para que no se vayan sobre escribiendo.
+
+  //     for(let item of response.items) {
+  //       //Podemos usar then y cach pero la anidación se puede tornar complicada de leer.
+  //       const url = await getDownloadURL(item); // Obtenemos la URL de descarga de cada objeto del arreglo de items.
+  //       this.images.push(url); //insertamos en el arreglo images cada una de las url traidas desde firebase.
+  //     }
+  //   })
+  //   .catch( error => console.log(error) )
+  } // Fin método Get
+
+} //Fin Clase
 
